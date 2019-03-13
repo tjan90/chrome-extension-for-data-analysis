b179857265083418c7c1fd8d82fb6c6307534059
Tanveer Jan <tjan90@yahoo.com>
2019-03-13 15:55:25 +0100

    Monday 12th March

--- background.js
+++ background.js
@@ -8,5 +8,7 @@ chrome.storage.local.get(null,function(result) {
    /* for (let i =0; i<json_array.length; i++){
         console.log(json_array[i]);
     }*/
-
+   for(let i = 0; i<json_array.length; i++){
+       console.log(json_array[i])
+   }
 });
